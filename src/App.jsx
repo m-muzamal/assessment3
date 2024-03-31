7c4da56d654bbe3250d3d342e83b8e487632352e
Muhammad Muzammal <muzamal503@gmail.com>
2024-03-31 09:21:56 +0500

    search-function

--- src/App.jsx
+++ src/App.jsx
@@ -8,60 +8,61 @@ import {
   useGetApi3Query,
 } from "./Redux/apiSlice/apiSlice";
 import { useDispatch } from "react-redux";
-import { addData } from "./Redux/dataSclice/dataSlice";
+import { addData, clearData } from "./Redux/dataSclice/dataSlice";
 import Footer from "./Components/footer/Footer";
 
 function App() {
   const [data, setData] = useState([]);
   const dispatch = useDispatch();
 
-  const api1 = useGetApi1Query();
-  const api2 = useGetApi2Query();
-  const api3 = useGetApi3Query();
+  const api1 = useGetApi1Query("author");
+  const api2 = useGetApi2Query("author");
+  const api3 = useGetApi3Query("newest");
 
   useEffect(() => {
-    if (api1.data && api2.data && api3.data) {
-      // console.log("API3", api3.data.response.results);
+    if (api1.data && api3.data) {
+      // console.log("API3", api1);
 
-      const data1 = api1.data.articles.map((article) => ({
+      const data1 = api1?.data.articles.map((article) => ({
         id: article.id,
         author: article.author,
         title: article.title,
-        disc: article.description,
-        img: article.urlToImage,
+        description: article.description,
+        urlToImage: article.urlToImage,
         url: article.url,
         category: article.source.name,
-        date: article.publishedAt,
+        publishedAt: article.publishedAt,
         source: "1",
       }));
 
-      const data2 = api2.data.articles.map((item) => ({
-        id: item.id,
-        author: item.author,
-        title: item.title,
-        disc: item.content,
-        img: item.urlToImage,
-        url: item.url,
-        category: item.source.name,
-        date: item.publishedAt,
-        source: "2",
-      }));
+      // const data2 = api2?.data.articles.map((item) => ({
+      //   id: item.id,
+      //   author: item.author,
+      //   title: item.title,
+      //   description: item.content,
+      //   urlToImage: item.urlToImage,
+      //   url: item.url,
+      //   category: item.source.name,
+      //   publishedAt: item.publishedAt,
+      //   source: "2",
+      // }));
 
-      const data3 = api3.data.response.results.map((item) => ({
+      const data3 = api3?.data.response.results.map((item) => ({
         id: item.id,
         author: item.pillarName,
-        disc: item.content,
+        description: item.content,
         url: item.webUrl,
         category: item.type,
         title: item.webTitle,
-        img: item.webImg,
-        date: item.webPublicationDate,
+        urlToImage: item.webImg,
+        publishedAt: item.webPublicationDate,
         source: "3",
       }));
 
       // console.log("Data3", data3);
 
-      const combinedData = [...data1, ...data2, ...data3];
+      // const combinedData = [...data3];
+      const combinedData = [...data1, ...data3];
       dispatch(addData(combinedData));
       setData(combinedData);
     }
